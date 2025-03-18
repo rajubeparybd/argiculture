3964f3d939dcd38bfe84c03e4f2263917fd9067c
Raju Bepary <raju174829@gmail.com>
2025-03-18 12:09:42 +0600

    fix: vercel error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/argiculture/',
+  build: {
+    outDir: "dist",
+  },
+  base: "/argiculture/",
   plugins: [react()],
 });
